ccd62a74c4aadfcdea88be91087237a295f99dd1
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2024-04-09 20:19:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,34 +12,31 @@ return res.json()}
 
     for(let i = 0; i<=9 ; i++){
         console.log(i)        
-        console.log(jsonBody.items[i])
-        console.log(jsonBody.items[i].data_publicacao)
+        console.log(jsonBody.items[i].link)
+        
 
         
         let div = document.createElement('div')
         let h1 = document.createElement('h1')
         h1.classList.add('titulo')
         let p = document.createElement('p')
+        let a = document.createElement('a')
         let span = document.createElement('span')
         span.classList.add('horario')
         p.classList.add('conteudo')
         
-        innerHTML = jsonBody.items[i].titulo
-        innerHTML = jsonBody.items[i].conteudo
-        innerHTML = jsonBody.items[i].horario
-        
         h1.innerHTML = jsonBody.items[i].titulo
         p.innerHTML = jsonBody.items[i].introducao
         span.innerHTML = jsonBody.items[i].data_publicacao
+        a.href = jsonBody.items[i].link
+        a.innerHTML = "saiba mais"
 
-
-
-        p.classList.add('conteudo')
         div.appendChild(span)
         div.appendChild(h1)
         div.appendChild(p)
+        div.appendChild(a)
+
         document.body.appendChild(div)
-        
         
     }
 })}
