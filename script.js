a8410912a3b8ab0f3828e13f5f897b30d8970587
Guilherme Antônio <127310615+guilhermeantonioo@users.noreply.github.com>
2024-04-09 20:20:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-const date = new Date()
-const dia = date.getDate()
-const mes = date.getMonth()-1
-const ano = date.getFullYear()
+*{
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+}
+h1{
+    font-size: 20px;
+}
+p{
+    font-size: 14px;
+}
+div{
+    margin: 0 auto 30px;
+    width: 70vw;
+    height: 30vh;
+    padding: 10px;
+    border-bottom: 1px solid;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    color: rgb(55, 55, 55);
+}
+header{
+    width: 100vw;
+    height: 5vh;
+    border-bottom: 2px solid;
+    box-shadow: 5px 2px 10px;
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 15px;
+}
+.logo{
+    width: 50px;
+    height: 50px;
+}
+a{
+    color: rgb(255, 0, 0);
+    text-decoration: none;
+    width: 100px;
+    padding: 4px;
+    margin-top: 10px;
+}
 
-const paramData = `${mes}-${dia}-${ano}`;
-
-function exibirNoticias (){
-fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?de=${paramData}`).then(res =>{
-return res.json()}   
-).then(jsonBody  =>{
-
-    for(let i = 0; i<=9 ; i++){
-        console.log(i)        
-        console.log(jsonBody.items[i].link)
-        
-
-        
-        let div = document.createElement('div')
-        let h1 = document.createElement('h1')
-        h1.classList.add('titulo')
-        let p = document.createElement('p')
-        let a = document.createElement('a')
-        let span = document.createElement('span')
-        span.classList.add('horario')
-        p.classList.add('conteudo')
-        
-        h1.innerHTML = jsonBody.items[i].titulo
-        p.innerHTML = jsonBody.items[i].introducao
-        span.innerHTML = jsonBody.items[i].data_publicacao
-        a.href = jsonBody.items[i].link
-        a.innerHTML = "saiba mais"
-
-        div.appendChild(span)
-        div.appendChild(h1)
-        div.appendChild(p)
-        div.appendChild(a)
-
-        document.body.appendChild(div)
-        
-    }
-})}
-exibirNoticias()
-
+div:hover{
+    background-color: #f6f6f6cf;
+    color: #000;
+}
